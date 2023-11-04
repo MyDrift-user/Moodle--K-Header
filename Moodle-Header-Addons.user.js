aa86db8aaa4d8c6a99a6ad1b5aca75a8d22da8e2
MyDrift <personal@mdiana.ch>
2023-11-04 15:53:54 +0100

    Update Moodle-Header-Addons.user.js

--- Moodle-Header-Addons.user.js
+++ Moodle-Header-Addons.user.js
@@ -5,8 +5,8 @@
 // @description  Adds a direct link to important sites on the Moodle header
 // @author       MyDrift (https://github.com/MyDrift-user/)
 // @match        https://moodle.bbbaden.ch/*
-// @downloadURL  https://github.com/MyDrift-user/EditColumnContent/blob/main/Moodle-Header-Addons.user.js
-// @updateURL    https://github.com/MyDrift-user/EditColumnContent/blob/main/Moodle-Header-Addons.user.js
+// @downloadURL  https://github.com/MyDrift-user/Moodle-Header-Addons/blob/main/Moodle-Header-Addons.user.js
+// @updateURL    https://github.com/MyDrift-user/Moodle-Header-Addons/blob/main/Moodle-Header-Addons.user.js
 // @grant        none
 // ==/UserScript==
 
